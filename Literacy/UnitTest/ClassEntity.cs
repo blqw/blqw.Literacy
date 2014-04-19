d72b6609de14862c24872d4cc4229222646046fd
jy02305022 <fttl_398@126.com>
2014-04-19 18:07:42 +0800

    添加单元测试

---
+++ Literacy/UnitTest/ClassEntity.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTest
+{
+    public class ClassEntity
+    {
+        //公共实例属性
+        public string ClassProperty { get; set; }
+        public int StructProperty { get; set; }
+
+        //公共静态属性
+        public static string StaticClassProperty { get; set; }
+        public static int StaticStructProperty { get; set; }
+
+        //私有实例属性
+        string PrivateClassProperty { get; set; }
+        int PrivateStructProperty { get; set; }
+
+        //私有静态属性
+        static string PrivateStaticClassProperty { get; set; }
+        static int PrivateStaticStructProperty { get; set; }
+
+        //公共实例字段
+        public string ClassField;
+        public int StructField;
+
+        //公共静态字段
+        public static string StaticClassField;
+        public static int StaticStructField;
+
+        //私有实例字段
+        string PrivateClassField;
+        int PrivateStructField;
+
+        //私有静态字段
+        static string PrivateStaticClassField;
+        static int PrivateStaticStructField;
+
+        //公共实例方法
+        public object[] Method(string str, int i)
+        {
+            ClassProperty = str;
+            StructProperty = i;
+            return new object[] { str, i };
+        }
+
+        public int MethodRef(ref string str, ref int i)
+        {
+            str += str;
+            i += i;
+            ClassProperty = str;
+            StructProperty = i;
+            return i;
+        }
+
+        public string MethodOut(out string str, out int i)
+        {
+            str = "A";
+            i = 1;
+            ClassProperty = str;
+            StructProperty = i;
+            return str;
+        }
+
+        //私有实例方法
+        object[] PrivateMethod(string str, int i)
+        {
+            ClassProperty = str;
+            StructProperty = i;
+            return new object[] { str, i };
+        }
+
+        int PrivateMethodRef(ref string str, ref int i)
+        {
+            str += str;
+            i += i;
+            ClassProperty = str;
+            StructProperty = i;
+            return i;
+        }
+
+        string PrivateMethodOut(out string str, out int i)
+        {
+            str = "A";
+            i = 1;
+            ClassProperty = str;
+            StructProperty = i;
+            return str;
+        }
+
+        //公共静态方法
+        public static object[] StaticMethod(string str, int i)
+        {
+            return new object[] { str, i };
+        }
+
+        public static int StaticMethodRef(ref string str, ref int i)
+        {
+            str += str;
+            i += i;
+            return i;
+        }
+
+        public static string StaticMethodOut(out string str, out int i)
+        {
+            str = "A";
+            i = 1;
+            return str;
+        }
+
+        //私有静态方法
+        static object[] PrivateStaticMethod(string str, int i)
+        {
+            return new object[] { str, i };
+        }
+
+        static int PrivateStaticMethodRef(ref string str, ref int i)
+        {
+            str += str;
+            i += i;
+            return i;
+        }
+
+        static string PrivateStaticMethodOut(out string str, out int i)
+        {
+            str = "A";
+            i = 1;
+            return str;
+        }
+    }
+}
---
+++ Literacy/UnitTest/UnitTest1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //测试引用类型的属性取值/赋值
+        [TestMethod]
+        public void TestClassProperty()
+        {
+            ClassEntity obj = new ClassEntity();
+
+            blqw.Literacy lit = new blqw.Literacy(obj.GetType());
+
+            object value;
+            //公共实例属性
+            lit.Property["ClassProperty"].SetValue(obj, "A");
+            value = lit.Property["ClassProperty"].GetValue(obj);
+            Assert.AreEqual(value, "A");
+
+            lit.Property["StructProperty"].SetValue(obj, 1);
+            value = lit.Property["StructProperty"].GetValue(obj);
+            Assert.AreEqual(value, 1);
+
+            //公共静态属性
+            lit.Load.StaticProperty(false);
+            lit.Property["StaticClassProperty"].SetValue(obj, "B");
+            value = lit.Property["StaticClassProperty"].GetValue(obj);
+            Assert.AreEqual(value, "B");
+
+            lit.Property["StaticStructProperty"].SetValue(obj, 2);
+            value = lit.Property["StaticStructProperty"].GetValue(obj);
+            Assert.AreEqual(value, 2);
+
+            //私有实例属性
+            lit.Load.NonPublicProperty();
+
+            lit.Property["PrivateClassProperty"].SetValue(obj, "C");
+            value = lit.Property["PrivateClassProperty"].GetValue(obj);
+            Assert.AreEqual(value, "C");
+
+            lit.Property["PrivateStructProperty"].SetValue(obj, 3);
+            value = lit.Property["PrivateStructProperty"].GetValue(obj);
+            Assert.AreEqual(value, 3);
+
+            //私有静态属性
+            lit.Load.StaticProperty(true);
+
+            lit.Property["PrivateStaticClassProperty"].SetValue(obj, "D");
+            value = lit.Property["PrivateStaticClassProperty"].GetValue(obj);
+            Assert.AreEqual(value, "D");
+
+            lit.Property["PrivateStaticStructProperty"].SetValue(obj, 4);
+            value = lit.Property["PrivateStaticStructProperty"].GetValue(obj);
+            Assert.AreEqual(value, 4);
+        }
+        //测试值类型的属性取值
+        [TestMethod]
+        public void TestStructProperty()
+        {
+            //值类型不支持成员赋值操作
+            StructEntity obj = new StructEntity("C", 3, "D", 4, null, 0, null, 0);
+            obj.ClassProperty = "A";
+            obj.StructProperty = 1;
+            StructEntity.StaticClassProperty = "B";
+            StructEntity.StaticStructProperty = 2;
+
+            blqw.Literacy lit = new blqw.Literacy(obj.GetType());
+
+            object value;
+            //公共实例属性
+            value = lit.Property["ClassProperty"].GetValue(obj);
+            Assert.AreEqual(value, "A");
+
+            value = lit.Property["StructProperty"].GetValue(obj);
+            Assert.AreEqual(value, 1);
+
+            //公共静态属性
+            lit.Load.StaticProperty(false);
+            value = lit.Property["StaticClassProperty"].GetValue(obj);
+            Assert.AreEqual(value, "B");
+
+            value = lit.Property["StaticStructProperty"].GetValue(obj);
+            Assert.AreEqual(value, 2);
+
+            //私有实例属性
+            lit.Load.NonPublicProperty();
+
+            value = lit.Property["PrivateClassProperty"].GetValue(obj);
+            Assert.AreEqual(value, "C");
+
+            value = lit.Property["PrivateStructProperty"].GetValue(obj);
+            Assert.AreEqual(value, 3);
+
+            //私有静态属性
+            lit.Load.StaticProperty(true);
+
+            value = lit.Property["PrivateStaticClassProperty"].GetValue(obj);
+            Assert.AreEqual(value, "D");
+
+            value = lit.Property["PrivateStaticStructProperty"].GetValue(obj);
+            Assert.AreEqual(value, 4);
+        }
+        //测试引用类型的字段取值/赋值
+        [TestMethod]
+        public void TestClassField()
+        {
+            ClassEntity obj = new ClassEntity();
+
+            blqw.Literacy lit = new blqw.Literacy(obj.GetType());
+
+            object value;
+            //公共实例字段
+            lit.Load.PublicField();
+            lit.Field["ClassField"].SetValue(obj, "A");
+            value = lit.Field["ClassField"].GetValue(obj);
+            Assert.AreEqual(value, "A");
+
+            lit.Field["StructField"].SetValue(obj, 1);
+            value = lit.Field["StructField"].GetValue(obj);
+            Assert.AreEqual(value, 1);
+
+            //公共静态字段
+            lit.Load.StaticField(false);
+            lit.Field["StaticClassField"].SetValue(obj, "B");
+            value = lit.Field["StaticClassField"].GetValue(obj);
+            Assert.AreEqual(value, "B");
+
+            lit.Field["StaticStructField"].SetValue(obj, 2);
+            value = lit.Field["StaticStructField"].GetValue(obj);
+            Assert.AreEqual(value, 2);
+
+            //私有实例字段
+            lit.Load.NonPublicField();
+
+            lit.Field["PrivateClassField"].SetValue(obj, "C");
+            value = lit.Field["PrivateClassField"].GetValue(obj);
+            Assert.AreEqual(value, "C");
+
+            lit.Field["PrivateStructField"].SetValue(obj, 3);
+            value = lit.Field["PrivateStructField"].GetValue(obj);
+            Assert.AreEqual(value, 3);
+
+            //私有静态字段
+            lit.Load.StaticField(true);
+
+            lit.Field["PrivateStaticClassField"].SetValue(obj, "D");
+            value = lit.Field["PrivateStaticClassField"].GetValue(obj);
+            Assert.AreEqual(value, "D");
+
+            lit.Field["PrivateStaticStructField"].SetValue(obj, 4);
+            value = lit.Field["PrivateStaticStructField"].GetValue(obj);
+            Assert.AreEqual(value, 4);
+        }
+        //测试值类型的字段取值
+        [TestMethod]
+        public void TestStructField()
+        {
+            //值类型不支持成员赋值操作
+            StructEntity obj = new StructEntity(null, 0, null, 0, "C", 3, "D", 4);
+            obj.ClassField = "A";
+            obj.StructField = 1;
+            StructEntity.StaticClassField = "B";
+            StructEntity.StaticStructField = 2;
+
+            blqw.Literacy lit = new blqw.Literacy(obj.GetType());
+
+            object value;
+            lit.Load.PublicField();
+            //公共实例字段
+            value = lit.Field["ClassField"].GetValue(obj);
+            Assert.AreEqual(value, "A");
+
+            value = lit.Field["StructField"].GetValue(obj);
+            Assert.AreEqual(value, 1);
+
+            //公共静态字段
+            lit.Load.StaticField(false);
+            value = lit.Field["StaticClassField"].GetValue(obj);
+            Assert.AreEqual(value, "B");
+
+            value = lit.Field["StaticStructField"].GetValue(obj);
+            Assert.AreEqual(value, 2);
+
+            //私有实例字段
+            lit.Load.NonPublicField();
+
+            value = lit.Field["PrivateClassField"].GetValue(obj);
+            Assert.AreEqual(value, "C");
+
+            value = lit.Field["PrivateStructField"].GetValue(obj);
+            Assert.AreEqual(value, 3);
+
+            //私有静态字段
+            lit.Load.StaticField(true);
+
+            value = lit.Field["PrivateStaticClassField"].GetValue(obj);
+            Assert.AreEqual(value, "D");
+
+            value = lit.Field["PrivateStaticStructField"].GetValue(obj);
+            Assert.AreEqual(value, 4);
+        }
+        //测试引用类型方法调用,参数包含值类型,引用类型, ref 和out,
+        [TestMethod]
+        public void TestClassMethod()
+        {
+            ClassEntity obj = new ClassEntity();
+            var methods = obj.GetType().GetMethods(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Static);
+
+            foreach (var method in methods)
+            {
+                string str = "Z";
+                int i = 26;
+                object[] args = new object[] { str, i };
+
+                var caller = blqw.Literacy.CreateCaller(method);
+                if (method.Name.EndsWith("Ref"))
+                {
+                    var val = caller(obj, args);
+                    obj.MethodRef(ref str, ref i);
+                    Assert.AreEqual(val, i);
+                }
+                else if (method.Name.EndsWith("Out"))
+                {
+                    var val = caller(obj, args);
+                    obj.MethodOut(out str, out i);
+                    Assert.AreEqual(val, str);
+                }
+                else if (method.Name.EndsWith("Method"))
+                {
+                    args = obj.Method(str, i);
+                    str = (string)args[0];
+                    i = (int)args[1];
+                    args = (object[])caller(obj, str, i);
+                }
+                else
+                {
+                    continue;
+                }
+
+                Assert.AreEqual(args[0], str);
+                Assert.AreEqual(args[1], i);
+                if (method.Name.Contains("Static") == false)
+                {
+                    Assert.AreEqual(obj.ClassProperty, str);
+                    Assert.AreEqual(obj.StructProperty, i);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestStructMethod()
+        {
+            StructEntity obj = new StructEntity();
+            var methods = obj.GetType().GetMethods(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Static);
+
+            foreach (var method in methods)
+            {
+                string str = "Z";
+                int i = 26;
+                object[] args = new object[] { str, i };
+
+                var caller = blqw.Literacy.CreateCaller(method);
+                if (method.Name.EndsWith("Ref"))
+                {
+                    var val = caller(obj, args);
+                    obj.MethodRef(ref str, ref i);
+                    Assert.AreEqual(val, i);
+                }
+                else if (method.Name.EndsWith("Out"))
+                {
+                    var val = caller(obj, args);
+                    obj.MethodOut(out str, out i);
+                    Assert.AreEqual(val, str);
+                }
+                else if (method.Name.EndsWith("Method"))
+                {
+                    args = obj.Method(str, i);
+                    str = (string)args[0];
+                    i = (int)args[1];
+                    args = (object[])caller(obj, str, i);
+                }
+                else
+                {
+                    continue;
+                }
+
+                Assert.AreEqual(args[0], str);
+                Assert.AreEqual(args[1], i);
+                if (method.Name.Contains("Static") == false)
+                {
+                    Assert.AreEqual(obj.ClassProperty, str);
+                    Assert.AreEqual(obj.StructProperty, i);
+                }
+            }
+        }
+    }
+}
